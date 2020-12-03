88781e55e5bba5295d7b2da58d5c74dea951d8bb
prowh08 <pro_wh08@naver.com>
2020-12-03 18:19:44 +0900

    [yjh]Page_Connect
    페이지 연결

---
+++ Artisan_s_Workplace/app/src/main/java/com/father_in_law/artisan_s_workplace/Talent_ShareActivity.java
@@ -0,0 +1,14 @@
+package com.father_in_law.artisan_s_workplace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Talent_ShareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_talent__share);
+    }
+}
